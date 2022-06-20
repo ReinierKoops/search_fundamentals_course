a5306982eca0901824d52fbb3531e07795df2afc
Reinier Koops <info@reinier.work>
2022-06-20 19:52:10 +0000

    Level 2

--- week2/search.py
+++ week2/search.py
@@ -106,8 +106,10 @@ def query():
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
         ##### W2, L1, S2
-
+        
         ##### W2, L2, S2
+        query_obj = qu.add_spelling_suggestions(query_obj=query_obj, user_query=user_query)
+        
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -123,6 +125,7 @@ def query():
         #### W2, L1, S2
 
         ##### W2, L2, S2
+        query_obj = qu.add_spelling_suggestions(query_obj=query_obj, user_query=user_query)
 
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
